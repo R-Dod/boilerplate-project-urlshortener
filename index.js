073e905780b0bcc686e4bb3a108ad118356f4eb4
Ritika Dodani <r.dodani_18551@khi.iba.edu.pk>
2023-06-17 22:44:49 +0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+const urls = require('./routes').urls
+const dns = require('node:dns');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +14,50 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/api/shorturl', function middleware(req, res, next) {
+  const url = new URL(req.body.url)
+  console.log(url.hostname, url.pathname);
+  dns.lookup(url.host, (err, address, family) => {
+    if (err) {
+      console.error(err);
+      req.origUrl = null;
+    } else {
+      req.origUrl = req.body.url;
+    }
+  });
+  next();
+},
+  function (req, res) {
+    console.log("origURL: ", req.origUrl);
+    let response;
+    if (req.origUrl === undefined) {
+      response = { error: "invalid url" };
+    }
+    else {
+      urls.push(req.origUrl);
+      const index = urls.length - 1;
+      response = { original_url: urls[index], short_url: index }
+    }
+    res.json(response);
+  });
+
+app.get('/api/shorturl/:id', function (req, res) {
+  res.redirect(urls[req.params.id]);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
